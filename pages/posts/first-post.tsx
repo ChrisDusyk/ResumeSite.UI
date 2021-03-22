f4a1cd01e945294896bb7d29cecf5e71558e939a
Chris Dusyk <chris.dusyk@gmail.com>
2021-03-22 14:19:32 -0600

    Playing with Next

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/Layout";
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>First post</h1>
             <Link href="/">Back home</Link>
-        </>
+        </Layout>
     );
 }
