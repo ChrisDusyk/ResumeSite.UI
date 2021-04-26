9a0cb6fcbe5ebc41e46df649e2a923624cebba4e
Chris Dusyk <chris.dusyk@gmail.com>
2021-04-26 10:52:46 -0600

    Set up Chakra UI

--- components/Layout.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./Layout.module.css";
-
-export default function Layout({ children }: React.PropsWithChildren<unknown>) {
-    return <div className={styles.container}>{children}</div>;
-}
